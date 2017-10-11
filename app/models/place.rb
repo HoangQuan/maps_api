d79f9580fb36298e5acde0443d4283f94c520096
Hoang Quan <quanhv@runsystem.net>
2017-10-11 09:54:03 +0700

    add map with multiple marker

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,16 @@ class Place < ApplicationRecord
   # after_validation :geocode
 
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+  reverse_geocoded_by :latitude, :longitude
+
+  # reverse_geocoded_by :latitude, :longitude, :address => :full_address
+
+  # reverse_geocoded_by :latitude, :longitude do |obj, results|
+  #   if geo = results.first
+  #     obj.city    = geo.city
+  #     obj.zipcode = geo.postal_code
+  #     obj.country = geo.country_code
+  #   end
+  # end
 end
